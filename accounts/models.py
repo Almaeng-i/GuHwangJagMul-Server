c31a297dbb1b2efb15403c3221d7ca749a23fd91
MinnSeoo <ms.rry01@gmail.com>
2024-04-02 11:02:06 +0900

    ⚡️ simple : SocialProfile 모델 생성 및 재정의

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from .managers import CustomUserManager
+from allauth.socialaccount.models import SocialAccount
 
 
 # Create your models here.
@@ -14,9 +15,18 @@ class CustomUser(AbstractUser):
 
     objects = CustomUserManager()
 
-    spouse_name = models.CharField(blank=True, max_length=100)
-    date_of_birth = models.DateField(blank=True, null=True)
-    
+    social_provider = models.CharField(max_length=100, blank=True)
+    social_uid = models.CharField(max_length=255, blank=True)
+    is_social_user = models.BooleanField(default=False)
 
     def __str__(self):
         return self.email
+
+class SocialProfile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    social_account = models.OneToOneField(SocialAccount, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.user.is_social_user = True  # 소셜 사용자로 표시
+        self.user.save()
+        super().save(*args, **kwargs)
