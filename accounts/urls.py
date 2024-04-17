6feaefc9184dd219455ab699c8270af908acfbad
MinnSeoo <ms.rry01@gmail.com>
2024-04-17 23:45:35 +0900

    ✅ : test 관련 url path 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from accounts import views
+from .middleware import AccessTokenMiddleware
 import jwt
 
 
 
 urlpatterns = [
     path('kakao/login/', views.kakao_login, name='kakao_login'),
-    path('login/kakao/callback/', views.KakaoCallbackView.as_view(), name='kakao_callback')
+    path('login/kakao/callback/', views.KakaoCallbackView.as_view(), name='kakao_callback'),
+    path('test-api/', views.test_api, name='test_api'),
 ]
