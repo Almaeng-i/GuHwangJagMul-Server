a7115dabcaac335839a34b22f531b81ff58a0737
MinnSeoo <ms.rry01@gmail.com>
2024-04-22 11:05:24 +0900

    🔥 : 예외처리 코드 삭제

--- accounts/middleware.py
+++ accounts/middleware.py
@@ -22,34 +22,19 @@ class AccessTokenMiddleware:
         # 클라이언트의 요청에서 헤더에서 AccessToken을 추출.
         authorization_header = request.headers.get('AUTHORIZATION')
         
-        try:
-            token = authorization_header  # 인가코드
+        token = authorization_header  # 인가코드
             
-            if token is None:
-                raise ValueError("토큰이 없습니다.")
-                
-            
-            # AccessToken을 복호화하여 사용자를 인증
-            payload = jwt.decode(token, secret_key, algorithms=algorithm)
-            
-            # payload에서 사용자 식별 정보를 추출
-            id = payload.get('user_id')
+        # AccessToken을 복호화하여 사용자를 인증
+        payload = jwt.decode(token, secret_key, algorithms=algorithm)
+        
+        # payload에서 사용자 식별 정보를 추출
+        id = payload.get('user_id')
 
-            # 추출된 사용자 식별 정보를 사용하여 사용자를 가져옴.
-            user = CustomUser.objects.get(id=id)
-            
-            # 인증이 성공하면 요청에 사용자를 할당.
-            request.user = user
+        # 추출된 사용자 식별 정보를 사용하여 사용자를 가져옴.
+        user = CustomUser.objects.get(id=id)
         
-        except ValueError as e:
-            error_message = str(e)
-            return JsonResponse({'error': error_message}) 
-        
-        except InvalidSignatureError:
-            return JsonResponse({'error' : '유효하지 않은 토큰 값 입니다.'})
-        
-        except ExpiredSignatureError:
-            return JsonResponse({'error' : '만료된 토큰 값 입니다.'})
+        # 인증이 성공하면 요청에 사용자를 할당.
+        request.user = user
 
         return  self.get_response(request)   # views를 거친후에 처리됨.
     
@@ -60,6 +45,5 @@ class AccessTokenMiddleware:
             if each_path in path:
                 return True
         return False
-        
     
 
