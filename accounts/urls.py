0d6851d339bc0e3d44074466de0fe7bcc8750644
MinnSeoo <ms.rry01@gmail.com>
2024-04-25 09:58:52 +0900

    🔥 : path의 name 필드 값 삭제

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ import jwt
 
 
 urlpatterns = [
-    path('kakao/login/', views.kakao_login, name='kakao_login'),
-    path('kakao/login/callback/', views.KakaoCallbackView.as_view(), name='kakao_callback'),
-    path('token/refresh/', views.reissue_token, name='reissue_token')
+    path('kakao/login/', views.kakao_login),
+    path('kakao/login/callback/', views.KakaoCallbackView.as_view()),
+    path('token/refresh/', views.reissue_token)
 ]
