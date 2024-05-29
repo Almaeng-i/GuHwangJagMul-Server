e712308dbc083f336331aa657a3e859842a57166
MinnSeoo <ms.rry01@gmail.com>
2024-05-29 23:08:26 +0900

    update: add userprofile query path

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,6 +4,7 @@ from userprofile import views
 
 urlpatterns = [
     path('', views.create_user_profile),
-    path('update/', views.update_user_profile)
+    path('my/', views.response_userprofile),
+    path('update/', views.update_user_profile),
 
 ]
