f1ccbc07457756ca3379830916396aced0eec255
정민서 <102645965+MinnSeoo@users.noreply.github.com>
2024-06-08 17:29:58 +0900

    update: add about s3 setting

--- GHJM/settings.py
+++ GHJM/settings.py
@@ -116,6 +116,8 @@ DEFAULT_PROFILE_URL = get_secret('DEFAULT_PROFILE_URL', secrets)
 RECEIVE_IMG_ENDPOINT = get_secret('RECEIVE_IMG_ENDPOINT', secrets)
 BASE_S3_URL = get_secret('BASE_S3_URL', secrets)
 
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = False     
