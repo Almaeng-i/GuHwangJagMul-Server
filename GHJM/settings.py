103c275166a692b91409ecfa0bab0f80e4899ae8
MinnSeoo <ms.rry01@gmail.com>
2024-06-11 09:04:32 +0900

    bug-fix: specify separate CI and local base dir paths

--- GHJM/settings.py
+++ GHJM/settings.py
@@ -15,7 +15,12 @@ from pathlib import Path
 from datetime import timedelta      # JWT 사용됨
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+if os.getenv('CI'):
+    BASE_DIR = Path(__file__).resolve().parent.parent
+else:
+    BASE_DIR = Path(__file__).resolve().parent
+
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
