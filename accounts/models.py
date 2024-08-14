32920e7d1c2d5adc7174fb80a315f79c8966c968
MinnSeoo <ms.rry01@gmail.com>
2024-08-14 15:04:35 +0900

    update: add device token field

--- accounts/models.py
+++ accounts/models.py
@@ -17,6 +17,8 @@ class CustomUser(AbstractUser):
     social_provider = models.CharField(max_length=100, blank=True)
     social_uid = models.CharField(max_length=255, blank=True)
     is_social_user = models.BooleanField(default=False)
+    
+    device_token = models.CharField(max_length=255, blank=True, null=True)
 
     def __str__(self):
         return self.email
